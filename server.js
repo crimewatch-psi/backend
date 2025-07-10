f41435a4759b773ff466daf64d51e0ff1e779606
ameliazsabrina <152615423+ameliazsabrina@users.noreply.github.com>
2025-07-11 03:54:48 +0700

    fix: add deployed fe

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://crimewatch-psi.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
