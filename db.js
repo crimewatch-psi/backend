fc107fc88e81dd1ee8cac55213568c44fc234a3f
ameliazsabrina <152615423+ameliazsabrina@users.noreply.github.com>
2025-07-11 02:29:43 +0700

    fix: change query again

--- db.js
+++ db.js
@@ -20,27 +20,27 @@ connection.connect((err) => {
   }
   console.log("Connected to database.");
 
-  const sqlFile = path.join(__dirname, "crimewatch_full.sql");
-  try {
-    let sql = fs.readFileSync(sqlFile, "utf8");
+  // const sqlFile = path.join(__dirname, "crimewatch_full.sql");
+  // try {
+  //   let sql = fs.readFileSync(sqlFile, "utf8");
 
-    sql = sql
-      .replace(/DELIMITER.*\n/g, "")
-      .replace(/\$\$/g, ";")
-      .replace(/;;\n/g, ";")
-      .replace(/\/\*.*?\*\//gs, "")
-      .replace(/--.*\n/g, "\n");
+  //   sql = sql
+  //     .replace(/DELIMITER.*\n/g, "")
+  //     .replace(/\$\$/g, ";")
+  //     .replace(/;;\n/g, ";")
+  //     .replace(/\/\*.*?\*\//gs, "")
+  //     .replace(/--.*\n/g, "\n");
 
-    connection.query(sql, (err, results) => {
-      if (err) {
-        console.error("Error initializing database schema:", err);
-        return;
-      }
-      console.log("Database schema initialized successfully");
-    });
-  } catch (err) {
-    console.error("Error reading SQL file:", err);
-  }
+  //   connection.query(sql, (err, results) => {
+  //     if (err) {
+  //       console.error("Error initializing database schema:", err);
+  //       return;
+  //     }
+  //     console.log("Database schema initialized successfully");
+  //   });
+  // } catch (err) {
+  //   console.error("Error reading SQL file:", err);
+  // }
 });
 
 module.exports = connection;
